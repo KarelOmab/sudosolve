67a6032998c6b9e58122691b46bb4f2bc387f5e1
Karel Tutsu <KarelOmab@gmail.com>
2022-12-16 22:22:56 -0500

    Update solve.py
    refactor code to oop

--- solve.py
+++ solve.py
@@ -1,3 +1,76 @@
+class Board:
+
+    rows = []
+    cols = []
+
+    def __init__(self, board):
+        self.board = board
+
+        for col in range(9):
+            vertical_row = [row[col] for row in self.board]
+            self.cols.append(vertical_row)
+        
+        for row in self.board:
+            self.rows.append(row)
+    
+    # evaluate all columns vertically
+    def eval_cols(self):
+        res = []
+        for col in self.cols:
+            self.eval_row(col)
+        return res 
+
+    # evaluate all rows horizontally
+    def eval_rows(self):
+        res = []
+        for row in self.rows:
+            res.append(self.eval_row(row))
+        return res  
+
+    # returns -1 on error
+    # returns 0 on incomplete
+    # return 1 on complete
+    def eval_row(self, arr):
+        #check for error (more than one of the same number)
+        is_incomplete = False
+        for a in arr:
+            if a != 0 and arr.count(a) > 1:
+                return -1
+            if a == 0:
+                is_incomplete = True
+        
+        return 0 if is_incomplete else 1   # complete 
+    
+    def eval_state(self):
+        if -1 in self.rows or -1 in self.cols:
+            return "invalid board"
+
+        if self.rows.count(1) == 9 and self.cols.count(1) == 9:
+            return "solved board"
+        else:
+            return "unsolved board"
+    
+    # write a function that takes in a board state and returns a solved board
+    # if the board is invalid, return an error
+    def solve_board(self):
+        while (self.eval_state() != "solved board"):
+            self.next_move()
+
+    def next_move(self):
+        # pick a cell that contains a number, attempt to solve it
+        pass
+
+    def __str__(self):
+        s = []
+        for row in self.rows:
+            buff = []
+            for col in row:
+                buff.append(str(col))
+            s.append("\t".join(buff))
+        return "\n\n".join(s)
+
+    
+
 # given a state of a sudoku board
 # 1. determine if the board is valid
 # 2. if the board is valid, find solution
@@ -39,66 +112,32 @@ solved = [
     [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ]
 
-# print board
-def print_board(board):
-    for row in board:
-        for col in row:
-            print(col, end=", ")
-        print("\n")
 
-# evaluate all columns vertically
-def eval_cols(board):
-    res = []
-    for col in range(9):
-        vertical_row = [row[col] for row in board]
-        res.append(eval_arr(vertical_row))
-    return res 
 
-# evaluate all rows horizontally
-def eval_rows(board):
-    res = []
-    for row in board:
-        res.append(eval_arr(row))
-    return res     
 
-# returns -1 on error
-# returns 0 on incomplete
-# return 1 on complete
-def eval_arr(arr):
-    #check for error (more than one of the same number)
-    is_incomplete = False
-    for a in arr:
-        if a != 0 and arr.count(a) > 1:
-            return -1
-        if a == 0:
-            is_incomplete = True
+
+
+
+
+
+
+                         
     
-    return 0 if is_incomplete else 1   # complete
-
-
-def eval_rows_cols(r, c):
-    if -1 in r or -1 in c:
-        print("invalid board")
-        return
-
-    if r.count(1) == 9 and c.count(1) == 9:
-        print("solved board")
-    else:
-        print("unsolved board")
 
 # driver code
-r = eval_rows(invalid)
-c = eval_cols(invalid)
-eval_rows_cols(r, c)
+#r = eval_rows(invalid)
+#c = eval_cols(invalid)
+#eval_rows_cols(r, c)
 
 
-r = eval_rows(solved)
-c = eval_cols(solved)
-eval_rows_cols(r, c)
+#r = eval_rows(solved)
+#c = eval_cols(solved)
+#eval_rows_cols(r, c)
 
-r = eval_rows(board)
-c = eval_cols(board)
-eval_rows_cols(r, c)
+b = Board(board)
+
+print(b)
+#print(b.eval_state())
    
 
 
